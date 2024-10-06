6e47971ae1e960d3457c018adb81c7b781b7d0a4
prissmarquez <a01738866@tec.mx>
2024-10-06 17:04:25 -0600

    agregar assests/images

--- lib/pages/introPage.dart
+++ lib/pages/introPage.dart
@@ -10,9 +10,26 @@ class introPage extends StatelessWidget {
       body: Column(
         children: [
           //logo 
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Image.asset(
+              "lib/images/nikelogo1.png",
+              height: 240,
+            
+              ),
+          ),
+
+          SizedBox(height: 20.0),
 
           //title
-
+          Text(
+            "Only walks to the future",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25
+            ) 
+            )
+          
           //sub title
 
           //start new button 
