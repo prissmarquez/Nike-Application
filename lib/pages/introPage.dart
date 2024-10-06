5fa5c8efefa106f338dd4717e5761271518854cd
prissmarquez <a01738866@tec.mx>
2024-10-06 17:26:23 -0600

    intro page

--- lib/pages/introPage.dart
+++ lib/pages/introPage.dart
@@ -6,34 +6,69 @@ class introPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body: Column(
-        children: [
-          //logo 
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Image.asset(
-              "lib/images/nikelogo1.png",
-              height: 240,
-            
+      backgroundColor: const Color.fromARGB(206, 222, 222, 222),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            children: [
+              SizedBox(height: 40,),
+              //logo 
+              Padding(
+                padding: const EdgeInsets.all(35.0),
+                child: Image.asset(
+                  "lib/images/nikelogo1.png",
+                  height: 240,
+                
+                  ),
               ),
-          ),
-
-          SizedBox(height: 20.0),
-
-          //title
-          Text(
-            "Only walks to the future",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 25
-            ) 
-            )
           
-          //sub title
-
-          //start new button 
-        ],
+              SizedBox(height: 15.0),
+          
+              //title
+              Text(
+                "Only walks to the future",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25
+                ) 
+                ),
+              
+              SizedBox(height: 30,),
+          
+              //sub title
+              Text(
+                "Welcome to Nikelab",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal
+                ),
+                textAlign: TextAlign.center,
+              ),
+        
+              SizedBox(height: 40,),
+        
+              //start button 
+              Container(
+                decoration:BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: EdgeInsets.all(25),
+                child: Center(
+                  child: Text(
+                    "Shop Now"
+                    ,style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16
+                  ),
+                              ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
