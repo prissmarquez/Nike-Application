d0d67344eb1e2a5e954eb97b3e5b4a48ca1c4ce7
prissmarquez <a01738866@tec.mx>
2024-10-06 16:22:15 -0600

    first commit

---
+++ lib/pages/introPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class introPage extends StatelessWidget {
+  const introPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Column(
+        children: [
+          //logo 
+
+          //title
+
+          //sub title
+
+          //start new button 
+        ],
+      ),
+    );
+  }
+}
