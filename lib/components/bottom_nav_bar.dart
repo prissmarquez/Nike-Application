0b1e1931c5ec3e83b2d2709a5deeae0d48294f6d
prissmarquez <a01738866@tec.mx>
2024-10-07 17:09:08 -0600

    introPage sin navigator

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  const MyBottomNavBar({super.key});
+  void Function(int)? onTabChange;
+  MyBottomNavBar({super.key, required this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: GNav(
+        activeColor: Colors.black,
+        tabActiveBorder: Border.all(color: Colors.white) ,
+        tabBackgroundColor: Colors.grey,
+        //mainAxisAlignment: MainAxisAlignment.center,
+        onTabChange: (value) => onTabChange!(value),
         tabs: [
         GButton(
           icon: Icons.home,
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:naiki/components/bottom_nav_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //this selected index is to control the button nav bar 
+  int _selectedIndex = 0;
+
+  //this method will update our detected index
+  // when the user tap on the button bar
+ void navigationbottonbar(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+ }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(206, 222, 222, 222),
-      bottomNavigationBar: MyBottomNavBar(),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigationbottonbar (index),
+      ),
     );
   }
 }
