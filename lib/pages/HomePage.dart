7d358c580e0a8c7cfc249b58ce1fed78c2515769
prissmarquez <a01738866@tec.mx>
2024-10-14 21:40:29 -0600

    menu drawer

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -38,6 +38,97 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigationbottonbar (index),
       ),
+
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Builder(
+          builder: (context) => IconButton(
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          }, 
+          icon: Icon
+          (
+            Icons.menu,
+            color: Colors.black,
+            )
+          )
+          )
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                //logo
+             DrawerHeader(child: Image.asset("lib/images/nikelogo.jpg")),
+
+             const Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: Divider(
+                color: Colors.black,
+              ),
+             ),
+
+             //another pages
+             //home
+             const Padding(
+              padding: const EdgeInsets.only(left:25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.white 
+                  ),
+                title: Text(
+                  'Home',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  ),
+              ),
+             ),
+
+             //about
+             const Padding(
+              padding: const EdgeInsets.only(left:25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.info_outline,
+                  color: Colors.white 
+                  ),
+                title: Text(
+                  'About',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  ),
+              ),
+             ),
+              ],
+            ),
+
+            //logout
+            const Padding(
+              padding: const EdgeInsets.only(left:25.0, bottom: 20),
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout_outlined,
+                  color: Colors.white 
+                  ),
+                title: Text(
+                  'Logout',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
       body: _pages[_selectedIndex],
     );
   }
